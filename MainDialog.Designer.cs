a05f914cea965c9ab8c76ec0d8a283f0407c351f
David Caravello <dcaravello@gmail.com>
2017-03-19 18:14:19 -0500

    Initial commit

---
+++ MainDialog.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace BatchImageWebOptimize
+{
+    partial class MainDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.sourceImageTextBox = new System.Windows.Forms.TextBox();
+            this.sourceImageBrowseButton = new System.Windows.Forms.Button();
+            this.outputImageBrowseButton = new System.Windows.Forms.Button();
+            this.outputImageTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.qualityTrackBar = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.optimzeButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.qualityLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.qualityTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Source Image Path:";
+            // 
+            // sourceImageTextBox
+            // 
+            this.sourceImageTextBox.Location = new System.Drawing.Point(116, 12);
+            this.sourceImageTextBox.Name = "sourceImageTextBox";
+            this.sourceImageTextBox.Size = new System.Drawing.Size(567, 20);
+            this.sourceImageTextBox.TabIndex = 4;
+            // 
+            // sourceImageBrowseButton
+            // 
+            this.sourceImageBrowseButton.Location = new System.Drawing.Point(689, 9);
+            this.sourceImageBrowseButton.Name = "sourceImageBrowseButton";
+            this.sourceImageBrowseButton.Size = new System.Drawing.Size(84, 23);
+            this.sourceImageBrowseButton.TabIndex = 5;
+            this.sourceImageBrowseButton.Text = "Browse";
+            this.sourceImageBrowseButton.UseVisualStyleBackColor = true;
+            this.sourceImageBrowseButton.Click += new System.EventHandler(this.sourceImageBrowseButton_Click);
+            // 
+            // outputImageBrowseButton
+            // 
+            this.outputImageBrowseButton.Location = new System.Drawing.Point(689, 35);
+            this.outputImageBrowseButton.Name = "outputImageBrowseButton";
+            this.outputImageBrowseButton.Size = new System.Drawing.Size(84, 23);
+            this.outputImageBrowseButton.TabIndex = 8;
+            this.outputImageBrowseButton.Text = "Browse";
+            this.outputImageBrowseButton.UseVisualStyleBackColor = true;
+            this.outputImageBrowseButton.Click += new System.EventHandler(this.outputImageBrowseButton_Click);
+            // 
+            // outputImageTextBox
+            // 
+            this.outputImageTextBox.Location = new System.Drawing.Point(116, 38);
+            this.outputImageTextBox.Name = "outputImageTextBox";
+            this.outputImageTextBox.Size = new System.Drawing.Size(567, 20);
+            this.outputImageTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output Image Path:";
+            // 
+            // qualityTrackBar
+            // 
+            this.qualityTrackBar.Location = new System.Drawing.Point(116, 79);
+            this.qualityTrackBar.Maximum = 100;
+            this.qualityTrackBar.Minimum = 10;
+            this.qualityTrackBar.Name = "qualityTrackBar";
+            this.qualityTrackBar.Size = new System.Drawing.Size(567, 45);
+            this.qualityTrackBar.TabIndex = 9;
+            this.qualityTrackBar.TickFrequency = 5;
+            this.qualityTrackBar.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.qualityTrackBar.Value = 75;
+            this.qualityTrackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Quality:";
+            // 
+            // optimzeButton
+            // 
+            this.optimzeButton.Location = new System.Drawing.Point(10, 117);
+            this.optimzeButton.Name = "optimzeButton";
+            this.optimzeButton.Size = new System.Drawing.Size(664, 102);
+            this.optimzeButton.TabIndex = 11;
+            this.optimzeButton.Text = "OPTIMIZE";
+            this.optimzeButton.UseVisualStyleBackColor = true;
+            this.optimzeButton.Click += new System.EventHandler(this.optimzeButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 226);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(663, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 12;
+            // 
+            // qualityLabel
+            // 
+            this.qualityLabel.AutoSize = true;
+            this.qualityLabel.Location = new System.Drawing.Point(690, 88);
+            this.qualityLabel.Name = "qualityLabel";
+            this.qualityLabel.Size = new System.Drawing.Size(0, 13);
+            this.qualityLabel.TabIndex = 13;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.progressLabel.Location = new System.Drawing.Point(680, 226);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(96, 23);
+            this.progressLabel.TabIndex = 14;
+            this.progressLabel.Text = "0 / 0";
+            this.progressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(680, 117);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(93, 102);
+            this.cancelButton.TabIndex = 15;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // MainDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 261);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.qualityLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.optimzeButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.qualityTrackBar);
+            this.Controls.Add(this.outputImageBrowseButton);
+            this.Controls.Add(this.outputImageTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sourceImageBrowseButton);
+            this.Controls.Add(this.sourceImageTextBox);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainDialog";
+            this.Text = "Batch Image Web Optimizer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainDialog_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.qualityTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sourceImageTextBox;
+        private System.Windows.Forms.Button sourceImageBrowseButton;
+        private System.Windows.Forms.Button outputImageBrowseButton;
+        private System.Windows.Forms.TextBox outputImageTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar qualityTrackBar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button optimzeButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label qualityLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
+
---
+++ MainDialog.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BatchImageWebOptimize
+{
+    public partial class MainDialog : Form
+    {
+        public MainDialog()
+        {
+            InitializeComponent();
+
+            sourceImageTextBox.Text = Properties.Settings.Default.sourcePath;
+            outputImageTextBox.Text = Properties.Settings.Default.outputPath;
+
+            updateQualityLabel();
+            //progressLabel.Parent = progressBar1;
+            //progressLabel.BackColor = Color.Transparent;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            updateQualityLabel();
+        }
+
+        private void updateQualityLabel()
+        {
+            this.qualityLabel.Text = qualityTrackBar.Value.ToString() + " %";
+        }
+
+        private void sourceImageBrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            folderBrowserDialog1.Description = "Select the directory that contains all the *.jpg images you would like to optimize";
+            folderBrowserDialog1.ShowNewFolderButton = true;
+
+            if (!String.IsNullOrEmpty(sourceImageTextBox.Text))
+                folderBrowserDialog1.SelectedPath = sourceImageTextBox.Text;
+            //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal;
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if( result == DialogResult.OK)
+            {
+                sourceImageTextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void outputImageBrowseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            folderBrowserDialog1.Description = "Select the directory to save optimized images (will overwrite files that already exist)";
+            folderBrowserDialog1.ShowNewFolderButton = false;
+
+            if (!String.IsNullOrEmpty(outputImageTextBox.Text))
+                folderBrowserDialog1.SelectedPath = outputImageTextBox.Text;
+            //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal;
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                outputImageTextBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        class GUIData
+        {
+            public string SourcePath;
+            public string OutputPath;
+            public int Quality;
+        }
+
+
+        private void optimzeButton_Click(object sender, EventArgs e)
+        {
+            //String outputPath = "C:\\deleteme\\imageResizeWorking\\working\\output";
+            //DirectoryInfo dirInfo = new DirectoryInfo("C:\\deleteme\\imageResizeWorking\\working\\");
+            
+            if (backgroundWorker1.IsBusy != true)
+            {
+                var guiData = new GUIData();
+                guiData.Quality = qualityTrackBar.Value;
+                guiData.SourcePath = sourceImageTextBox.Text;
+                guiData.OutputPath = outputImageTextBox.Text;
+
+                if( String.IsNullOrEmpty(guiData.SourcePath) || !Directory.Exists(guiData.SourcePath) )
+                {
+                    MessageBox.Show("Please provide a valid source path");
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(guiData.OutputPath) || !Directory.Exists(guiData.SourcePath))
+                {
+                    MessageBox.Show("Please provide an output path");
+                    return;
+                }
+
+                updateGUIWorkInProgress(true);
+                backgroundWorker1.RunWorkerAsync(guiData);
+            }
+        }
+
+        private IEnumerable<FileInfo> GetFilesByExtensions(DirectoryInfo dirInfo, params string[] extensions)
+        {
+            var allowedExtensions = new HashSet<string>(extensions, StringComparer.OrdinalIgnoreCase);
+
+            return dirInfo.EnumerateFiles().Where(f => allowedExtensions.Contains(f.Extension));
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var guiData = (GUIData)e.Argument;
+                BackgroundWorker worker = sender as BackgroundWorker;
+
+                String pathToImageMagick = AppDomain.CurrentDomain.BaseDirectory + "im\\";
+                String pathToConvertExe = pathToImageMagick + "convert.exe";
+                String sourcePath = sourceImageTextBox.Text;
+                String outputPath = guiData.OutputPath;
+
+                if (!String.IsNullOrEmpty(outputPath) && !outputPath.EndsWith("\\"))
+                    outputPath += "\\";
+
+                int quality = guiData.Quality;
+
+                if (!File.Exists(pathToConvertExe))
+                {
+                    MessageBox.Show("Image Magick executable not found at: " + pathToConvertExe);
+                    exitApplication();
+                    return;
+                }
+
+                Console.WriteLine("Image Magick Location: " + pathToConvertExe);
+
+                DirectoryInfo dirInfo = new DirectoryInfo(guiData.SourcePath);
+
+                IEnumerable<FileInfo> files = GetFilesByExtensions(dirInfo, ".jpg");
+
+                int numFilesToConvert = files.Count();
+                int currentFileIndex = 0;
+
+                if(numFilesToConvert == 0)
+                {
+                    MessageBox.Show("No source image files found");
+                    return;
+                }
+
+                foreach (FileInfo file in files)
+                {
+                    if (worker.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+
+                    currentFileIndex++;
+                    string sourceFileName = file.FullName;
+                    string fileName = file.Name;
+                    string fileNameNoExt = fileName.Substring(0, fileName.Length - file.Extension.Length);
+                    string destinationFileName = outputPath + fileNameNoExt + "_q" + quality + file.Extension;
+
+                    Console.WriteLine("howdy");
+
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = pathToConvertExe;
+                    proc.StartInfo.Arguments = sourceFileName + " -quality " + quality + " " + destinationFileName;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.CreateNoWindow = true;
+                    proc.Start();
+
+
+                    StreamReader srO = proc.StandardOutput;
+                    string standardOutput = srO.ReadToEnd();
+
+                    StreamReader srE = proc.StandardError;
+                    string standardError = srE.ReadToEnd();
+
+                    proc.WaitForExit();
+
+                    Console.WriteLine("Standard Output: ");
+                    Console.WriteLine(standardOutput);
+                    Console.WriteLine("Standard Error: ");
+                    Console.WriteLine(standardError);
+
+                    string statusText = currentFileIndex + " / " + numFilesToConvert;
+
+                    worker.ReportProgress((int)((currentFileIndex / (double)numFilesToConvert) * 100), statusText);
+                }
+            }
+            catch(Exception ex)
+            {
+                e.Cancel = true;
+                MessageBox.Show("Error occured: " + ex);
+                exitApplication();
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int newValue = e.ProgressPercentage;
+
+            if ( newValue + 1 <= 100 )
+            {
+                progressBar1.Value = (newValue*10) + 1;
+                progressBar1.Value = (newValue*10);
+            }
+            else
+            {
+                progressBar1.Value = 1000;
+                progressBar1.Value = 999;
+                progressBar1.Value = 1000;
+            }
+            
+            progressLabel.Text = e.UserState as string;
+        }
+
+        private void updateGUIWorkInProgress(bool workInProgress)
+        {
+            if(workInProgress)
+            {
+                sourceImageTextBox.Enabled = false;
+                sourceImageBrowseButton.Enabled = false;
+                outputImageTextBox.Enabled = false;
+                outputImageBrowseButton.Enabled = false;
+                qualityTrackBar.Enabled = false;
+                optimzeButton.Enabled = false;
+                cancelButton.Enabled = true;
+            }
+            else
+            {
+                sourceImageTextBox.Enabled = true;
+                sourceImageBrowseButton.Enabled = true;
+                outputImageTextBox.Enabled = true;
+                outputImageBrowseButton.Enabled = true;
+                qualityTrackBar.Enabled = true;
+                optimzeButton.Enabled = true;
+                cancelButton.Enabled = false;
+            }
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            updateGUIWorkInProgress(false);
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void exitApplication()
+        {
+            Application.Exit();
+        }
+
+        private void saveSettings()
+        {
+            Properties.Settings.Default.sourcePath = sourceImageTextBox.Text;
+            Properties.Settings.Default.outputPath = outputImageTextBox.Text;
+            Properties.Settings.Default.Save();
+            Console.WriteLine("Settings Saved");
+        }
+
+        private void MainDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveSettings();
+        }
+    }
+}
